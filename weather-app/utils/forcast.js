a0d16cabc54318464ec4e8f507e2fddd32518f20
Benjamin Huang <bhuang@rms.com>
2019-04-08 16:05:58 -0700

    mongo and docker

--- weather-app/utils/forcast.js
+++ weather-app/utils/forcast.js
@@ -1,7 +1,7 @@
 import { request } from "request";
 
 const forecast = (latitude, longitude, callback)=>{
-  const url = "https://api.darksky.net/";
+  const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
   request({url: url, json:true}, (error, response)=>{
     if(error)
     {
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,7 +1,7 @@
 import { request } from "request";
 
 const geocode = (address, callback)=>{
-  const url = "https://api.mapbox.com/"+address;
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
   request({url: url, json:true}, (error, response)=>{
     if(error)
     {
