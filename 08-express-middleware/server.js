73d4d7429092224a26fb4687b78f17eaae7d1611
Benjamin Huang <bhuang@rms.com>
2019-04-09 23:18:08 -0700

    express middleware sample

---
+++ 08-express-middleware/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+//
+const port = 3333
+
+//
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  if (req.method === 'GET') {
+    res.send('GET requests are disabled.');
+  }
+  else {
+    next();  // without this line, server will keep loading
+  }
+})
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
