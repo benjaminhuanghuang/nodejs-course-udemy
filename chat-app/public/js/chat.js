ed4b5174afe8f8a2ad9bf95e0539fa73097befe1
Benjamin Huang <bhuang@rms.com>
2019-04-11 15:53:08 -0700

    update

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -11,20 +11,20 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
-
 socket.on('message', (message) => {
     console.log(message)
-    // Using template
     const html = Mustache.render(messageTemplate, {
-        message
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('locationMessage', (url) => {
-    console.log(url)
+socket.on('locationMessage', (message) => {
+    console.log(message)
     const html = Mustache.render(locationMessageTemplate, {
-        url
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
